911a394dff23ab98a14796e6cf4dd68b42535574
magda.castro <magda.castro@mjv.com.br>
2023-07-11 20:02:55 -0300

    insert data in database

--- src/main/java/com/devsuperior/gamelist/entities/Game.java
+++ src/main/java/com/devsuperior/gamelist/entities/Game.java
@@ -24,7 +24,11 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
